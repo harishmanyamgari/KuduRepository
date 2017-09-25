ab698e11c04f22e44b6f6310ec41d9e2702ae7e8
Harish K Manyamgari <hmanyamgar001@nam.ad.pwcinternal.com>
2017-09-25 16:52:24 -0400

    mod home

--- KuduAzureApp/Controllers/HomeController.cs
+++ KuduAzureApp/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace KuduAzureApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Kudu application description page.";
 
             return View();
         }
